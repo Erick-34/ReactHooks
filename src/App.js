1d48f2378b5fff189614b3aee04c7f62b0f405ea
Erick-34 <erickoliveira2060@gmail>
2020-07-25 12:24:58 -0300

    done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,41 @@
-import React from 'react'
-
-import Home from './components/Home'
+import React, { useState, useEffect } from "react";
 
 function App() {
+  const [repositores, setRepositoris] = useState([]);
+
+
+  useEffect(async () => {
+    const response = await fetch("https://api.github.com/users/Erick-34/repos")
+    const data = await response.json()
+
+    setRepositoris(data);
+  }, []);
+
+  useEffect(() => {
+     const filtered = repositores.filter(repo => repo.favorite)
+     
+     document.title = `Você têm ${filtered.length} favoritos`
+  }, [repositores])
+
+  function handleFavorite(id) {
+      const newRepositories = repositores.map(repo => {
+         return repo.id === id ? { ...repo, favorite: !repo.favorite } : repo;
+      });
+
+      setRepositoris(newRepositories)
+  }
   return (
-    <div className="App">
-       <Home />
-    </div>
+    <>
+      <ul>
+        {repositores.map(repo => (
+          <li key={repo.id}>
+             {repo.name}
+             {repo.favorite && <span>(Favorite)</span>}
+             <button onClick={() => handleFavorite(repo.id)}>Favorite</button>
+          </li>
+        ))}
+      </ul>
+    </>
   );
 }
 
